e7a31e2c9dfd90d08de31dfb786089c554a75492
Jürgen Degenfellner <jdegenfellner@gmail.com>
2026-08-12 13:30:13 +0200

    Absolute numbers for the cv sensitivity as well

--- R/sample_size.R
+++ R/sample_size.R
@@ -101,8 +101,8 @@ n_r05 <- 2 * (qnorm(1 - alpha / 2) + qnorm(power_t))^2 *
   sigma^2 * (1 - 0.5^2) / delta^2
 out("\nWith r = 0.5 instead of 0.6, ICC 0.01, cv 0.65: ",
     ceiling(n_r05 * design_effect(m_bar, 0.01, 0.65, k = 13)), " per arm")
-out("With cv = 0 instead of 0.65, ICC 0.03, r = 0.6:  ",
-    ceiling(n_exact * design_effect(m_bar, 0.03, 0, k = 13)), " per arm")
+out("With cv = 0 instead of 0.65, ICC 0.01, r = 0.6:  ",
+    ceiling(n_exact * design_effect(m_bar, 0.01, 0, k = 13)), " per arm")
 
 # ---------------------------------------------------------------------
 # 4. Power of the design as it currently stands
