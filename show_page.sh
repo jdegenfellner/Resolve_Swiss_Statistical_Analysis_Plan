feb267d2eabe27773eb22041aff305d8ff331bb2
Jürgen Degenfellner <jdegenfellner@gmail.com>
2026-08-17 12:23:13 +0200

    Default to window-width zoom, extra steps opt-in

--- show_page.sh
+++ show_page.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Opens paper.pdf in Preview at a given page, fitted to the window.
+# Opens paper.pdf in Preview at a given page, on the left of the screen,
+# zoomed so the text is comfortable to read.
 #
 #   ./show_page.sh 13                 page number
 #   ./show_page.sh "Kenward"          first page containing that text
@@ -7,7 +8,9 @@
 # The document is closed first, because Preview restores the previous
 # scroll position when it reopens a file that is already open, which
 # overrides the jump. Menu items are clicked by name rather than by
-# keyboard shortcut, which turned out to be the unreliable part.
+# keyboard shortcut, which turned out to be the unreliable part, and the
+# page jump comes last, because changing view mode or zoom scrolls back
+# to the top.
 set -e
 cd "$(dirname "$0")"
 PDF="$(pwd)/paper.pdf"
@@ -21,6 +24,11 @@ case "$PAGE" in
 esac
 [ -z "$PAGE" ] && PAGE=1
 
+# window width as a fraction of the screen, and extra zoom steps beyond
+# "Zoom to Fit" (0 keeps the page at window width)
+WIDTH_FRACTION=${WIDTH_FRACTION:-0.55}
+ZOOM_STEPS=${ZOOM_STEPS:-0}
+
 osascript <<EOF >/dev/null
 tell application "Preview"
   try
@@ -33,11 +41,27 @@ tell application "Preview"
   activate
 end tell
 delay 1.5
+
+tell application "Finder" to set screenSize to bounds of window of desktop
+set screenW to item 3 of screenSize
+set screenH to item 4 of screenSize
+set winW to (screenW * $WIDTH_FRACTION) as integer
+
 tell application "System Events" to tell process "Preview"
+  set position of window 1 to {0, 25}
+  set size of window 1 to {winW, screenH - 25}
+  delay 0.5
   click menu item "Continuous Scroll" of menu 1 of menu bar item "View" of menu bar 1
   delay 0.3
   click menu item "Zoom to Fit" of menu 1 of menu bar item "View" of menu bar 1
-  delay 0.4
+  delay 0.3
+  if $ZOOM_STEPS > 0 then
+    repeat $ZOOM_STEPS times
+      click menu item "Zoom In" of menu 1 of menu bar item "View" of menu bar 1
+      delay 0.2
+    end repeat
+  end if
+  delay 0.3
   click menu item "Go to Page…" of menu 1 of menu bar item "Go" of menu bar 1
   delay 0.6
   keystroke "$PAGE"
